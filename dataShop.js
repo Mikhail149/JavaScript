fe46a45a79487b2034327c3ad3fe102a598c57c9
Mikhail149 <78578063+Mikhail149@users.noreply.github.com>
2021-04-12 21:13:49 +0300

    Add files via upload

---
+++ dataShop.js
@@ -0,0 +1,49 @@
+products = {
+    quantity: 6,
+    output:{
+        start: 1,
+        stop: 3
+    },
+    product_1: {
+        name: "Черный продукт",
+        money: 100,
+        description: "очень хороший товар",
+        fileName: "Black.png",
+        inBasket: 0
+    },
+    product_2: {
+        name: "Синий продукт",
+        money: 100,
+        description: "очень хороший товар",
+        fileName: "Blue.png",
+        inBasket: 0
+    },
+    product_3: {
+        name: "Зеленый продукт",
+        money: 100,
+        description: "очень хороший товар",
+        fileName: "Green.png",
+        inBasket: 0
+    },
+    product_4: {
+        name: "Красный продукт",
+        money: 100,
+        description: "очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень хороший товар",
+        fileName: "Red.png",
+        inBasket: 0
+    },
+    product_5: {
+        name: "Желтый продукт",
+        money: 100,
+        description: "очень хороший товар",
+        fileName: "Yellow.png",
+        inBasket: 0
+    },
+    product_6: {
+        name: "Розовый продукт",
+        money: 100,
+        description: "очень хороший товар",
+        fileName: "Purpur.png",
+        inBasket: 0
+    }
+};
